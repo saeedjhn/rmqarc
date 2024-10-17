90cc87c6fc4054b9fb0f3d8a9b6ee8c13fbdd7ba
saeedjhn <saeedjahanakbari@gmail.com>
2024-10-17 15:31:17 +0330

    chore: Define subscriber interface for handle receivers

---
+++ pkg/comms/subscriber_interface.go
@@ -0,0 +1,10 @@
+package comms
+
+import "context"
+
+// Subscriber is an interface that defines the Subscribe method to process incoming messages.
+type Subscriber interface {
+	// Subscribe processes the incoming Message.
+	// It takes a context and the Message object, and returns an error if processing fails.
+	Subscribe(ctx context.Context, msg Message) error
+}
