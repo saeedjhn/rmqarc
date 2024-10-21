3856019279f2a74d05ac3fce13978ce4c714b008
saeedjhn <saeedjahanakbari@gmail.com>
2024-10-21 12:42:03 +0330

    chore: Added doc file

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package rmqarc provides functionalities for managing RabbitMQ connections,
+// publishing messages, subscribing to queues, and automatically handling message deliveries.
+package rmqarc
